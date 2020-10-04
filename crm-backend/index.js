bfc9952625295d79bd02fcc6b3b8e762741ac903
swetabh <swetabh@nanamuna.com>
2020-10-04 20:02:26 +0530

    added gitignore for node_modules and yarn

--- crm-backend/index.js
+++ crm-backend/index.js
@@ -102,6 +102,6 @@ app.delete("/allusers/:id", async (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
 });
